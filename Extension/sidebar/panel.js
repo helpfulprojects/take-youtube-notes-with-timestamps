d4d42ae2f548d3e0e7537f014fabff71e9d41a63
Helpful Projects <helpfulprojectsgithub@gmail.com>
2024-04-26 15:42:22 +0300

    add input and detect enter press

--- Extension/sidebar/panel.js
+++ Extension/sidebar/panel.js
@@ -1,37 +1,12 @@
 let myWindowId;
-const contentBox = document.querySelector("#content");
+const currentMark = document.querySelector("#currentMark");
 
-/*
-Make the content box editable as soon as the user mouses over the sidebar.
-*/
-window.addEventListener("mouseover", () => {
-  contentBox.setAttribute("contenteditable", true);
+currentMark.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    console.log("Enter key pressed!");
+  }
 });
 
-/*
-When the user mouses out, save the current contents of the box.
-*/
-window.addEventListener("mouseout", () => {
-  contentBox.setAttribute("contenteditable", false);
-  browser.tabs.query({ windowId: myWindowId, active: true }).then((tabs) => {
-    let contentToStore = {};
-    console.log(contentBox);
-    let text = "";
-    if (contentBox.innerText) {
-      text = contentBox.innerText;
-    }
-    contentToStore[tabs[0].url] = text;
-    browser.storage.local.set(contentToStore);
-  });
-});
-
-/*
-Update the sidebar's content.
-
-1) Get the active tab in this sidebar's window.
-2) Get its stored content.
-3) Put it in the content box.
-*/
 function updateContent() {
   browser.tabs
     .query({ windowId: myWindowId, active: true })
@@ -39,25 +14,14 @@ function updateContent() {
       return browser.storage.local.get(tabs[0].url);
     })
     .then((storedInfo) => {
-      console.log(contentBox.textContent);
-      contentBox.innerText = storedInfo[Object.keys(storedInfo)[0]];
+      //contentBox.innerText = storedInfo[Object.keys(storedInfo)[0]];
     });
 }
 
-/*
-Update content when a new tab becomes active.
-*/
 browser.tabs.onActivated.addListener(updateContent);
 
-/*
-Update content when a new page is loaded into a tab.
-*/
 browser.tabs.onUpdated.addListener(updateContent);
 
-/*
-When the sidebar loads, get the ID of its window,
-and update its content.
-*/
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
   myWindowId = windowInfo.id;
   updateContent();
